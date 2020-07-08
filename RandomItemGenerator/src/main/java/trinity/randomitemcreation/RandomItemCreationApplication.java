4dd936d67296a32f8317fb88fcfbb802f5f96404
Pavdrew <33010073+Pavdrew@users.noreply.github.com>
2020-07-08 11:09:19 -0600

    ItemGenerator
    Randomly creates items based on the amount provided when querying it

---
+++ RandomItemGenerator/src/main/java/trinity/randomitemcreation/RandomItemCreationApplication.java
@@ -0,0 +1,16 @@
+package trinity.randomitemcreation;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RandomItemCreationApplication {
+
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(RandomItemCreationApplication.class, args);
+    }
+
+}
---
+++ RandomItemGenerator/src/test/java/trinity/randomitemcreation/RandomItemCreationApplicationTests.java
@@ -0,0 +1,13 @@
+package trinity.randomitemcreation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RandomItemCreationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
