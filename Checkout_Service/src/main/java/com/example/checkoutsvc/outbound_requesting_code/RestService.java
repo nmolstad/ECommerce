fce708903ae0ae7de1f784b1dedcd420b18f2412
Brax132 <42818491+Brax132@users.noreply.github.com>
2020-07-09 11:08:39 -0600

    Non-Tested CheckoutService

---
+++ Checkout_Service/src/main/java/com/example/checkoutsvc/outbound_requesting_code/RestService.java
@@ -0,0 +1,46 @@
+package com.example.checkoutsvc.outbound_requesting_code;
+
+import com.example.checkoutsvc.Email;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.*;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+public class RestService {
+
+    private final RestTemplate restTemplate;
+
+    public RestService(RestTemplateBuilder restTemplateBuilder) {
+        this.restTemplate = restTemplateBuilder.build();
+    }
+
+    public Email sendEmail(String url, String to) {
+//        String url = "https://jsonplaceholder.typicode.com/posts";
+
+        // create headers
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+
+        Map<String, Object> map = new HashMap<>();
+        map.put("message", "Thank you for your order");
+        map.put("to", to);
+
+        // build the request
+        HttpEntity<Map<String, Object>> entity = new HttpEntity<>(map, headers);
+
+        // send POST request
+        ResponseEntity<Email> response = this.restTemplate.postForEntity(url, entity, Email.class);
+
+        if (response.getStatusCode() == HttpStatus.CREATED) {
+            return response.getBody();
+        } else {
+            return null;
+        }
+    }
+}
